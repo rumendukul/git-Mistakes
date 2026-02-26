d7c4370ddcc7791bb0e7df45ef0d9299cc489104
Aruuke Mendukulova <aruukemendukulova@MacBookPro.lan>
2026-02-26 12:06:56 -0600

    added sayHello method

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld{
      public static void main (String[]args){
         System.out.println("Hello World");
      }
+     public String sayHello(){
+         this is error needs to be fixed;
+         return "Hello";
+     }    
 }
